5ba33eb7a00ddad3d631410fefda112817754568
Joseph Mate <mate.joseph@gmail.com>
2014-04-07 21:40:26 -0400

    added display of a single join

--- src/backend/ui/optimizer_ui.c
+++ src/backend/ui/optimizer_ui.c
@@ -61,8 +61,6 @@
  */
 struct UIState {
 	int height;
-	int rows;
-	int cols;
 	PlannerInfo *root;
 	Path **cheapest_path;
 	List* nodeStatesToFree;
@@ -95,106 +93,171 @@ static void destroy(GtkWidget* widget, gpointer data) {
 	gtk_main_quit();
 }
 
-static int compute_height(Path* path_so_far) {
-	if(path_so_far == NULL) {
-		return 0;
+static bool isJoinPath(Path* path) {
+	if(path == NULL) {
+		return false;
 	}
-	switch(path_so_far->pathtype){
+	switch(path->pathtype){
+		case T_HashJoin:
+		case T_MergeJoin:
+		case T_NestLoop:
+			return true;
+		default:
+			return false;
+	}
+}
+
+static bool isLeafPath(Path* path) {
+	if(path == NULL) {
+		return false;
+	}
+	switch(path->pathtype){
 		case T_SeqScan:
 		case T_IndexOnlyScan :
 		case T_IndexScan :
-			return 1;
+			return true;
 		default:
-			return -1;
+			return false;
+	}
+}
+
+static int compute_height(Path* path_so_far) {
+	if(path_so_far == NULL) {
+		return 0;
+	} else if(isJoinPath(path_so_far)) {
+		int best,right;
+		JoinPath * joinPath;
+		joinPath = (JoinPath *)path_so_far;
+		best = compute_height(joinPath->outerjoinpath);
+		right = compute_height(joinPath->innerjoinpath);
+		if(right > best) {
+			best = right;
+		}
+		return best + 1;
+	} else if(isLeafPath(path_so_far)) {
+		return 1;
+	} else {
+		return -10000000;
 	}
 }
 
 #define CSTR_BUFF 2048
 
-static GtkWidget * create_seq_scan_widget(UIState * state, Path * seqscan) {
+static GtkWidget * create_widget_from_path(
+		UIState * state,
+		Path * seqscan,
+		char * typeStr,
+		void (*onclick)(GtkWidget*, gpointer)
+		) {
 	GtkGrid *grid;
-	GtkWidget *lblSeqScan;
+	GtkWidget *lbl;
 	GtkWidget *lblInfo;
-	GtkWidget *btnSeqScan;
+	GtkWidget *btn;
 	RelOptInfo *rel;
 	char * buff;
 
 	buff = malloc(sizeof(char)*CSTR_BUFF);
 	rel = seqscan->parent;
 	cstr_rel(buff, 0, CSTR_BUFF-1, state->root, rel);
-	printf("Sequence Scan\n");
-	printf("%s\n", buff);
 	grid = (GtkGrid*)gtk_grid_new();
-	lblSeqScan = gtk_label_new( "Sequence Scan" );
+	lbl = gtk_label_new( typeStr );
 	lblInfo = gtk_label_new( buff );
-	btnSeqScan = gtk_button_new_with_label("update");
-	g_signal_connect(G_OBJECT(btnSeqScan), "clicked", G_CALLBACK(btn_clicked), NULL);
-	gtk_grid_attach(grid, lblSeqScan, 0,0,1,1);
+	btn = gtk_button_new_with_label("update");
+	g_signal_connect(G_OBJECT(btn), "clicked", G_CALLBACK(*onclick), NULL);
+	gtk_grid_attach(grid, lbl, 0,0,1,1);
 	gtk_grid_attach(grid, lblInfo, 0,1,1,1);
-	gtk_grid_attach(grid, btnSeqScan, 0,2,1,1);
+	gtk_grid_attach(grid, btn, 0,2,1,1);
 
-	gtk_widget_show(GTK_WIDGET(lblSeqScan));
+	gtk_widget_show(GTK_WIDGET(lbl));
 	gtk_widget_show(GTK_WIDGET(lblInfo));
-	gtk_widget_show(GTK_WIDGET(btnSeqScan));
+	gtk_widget_show(GTK_WIDGET(btn));
 	gtk_widget_show(GTK_WIDGET(grid));
 	return GTK_WIDGET(grid);
+}
+
+static GtkWidget * create_hash_join_widget(UIState * state, JoinPath * joinpath) {
+	return GTK_WIDGET(
+			create_widget_from_path(state, &joinpath->path, "Hash Join", &btn_clicked)
+		);
+}
+
+static GtkWidget * create_merge_join_widget(UIState * state, JoinPath * joinpath) {
+	return GTK_WIDGET(
+			create_widget_from_path(state, &joinpath->path, "Merge Join", &btn_clicked)
+		);
+}
+
+static GtkWidget * create_nested_loop_widget(UIState * state, JoinPath * joinpath) {
+	return GTK_WIDGET(
+			create_widget_from_path(state, &joinpath->path, "Nested Loop", &btn_clicked)
+		);
+}
+
+static GtkWidget * create_seq_scan_widget(UIState * state, Path * seqscan) {
+	return GTK_WIDGET(
+			create_widget_from_path(state, seqscan, "Sequence Scan", &btn_clicked)
+		);
 }
 
 static GtkWidget * create_idx_scan_widget(UIState * state, Path * idxscan) {
-	GtkGrid *grid;
-	GtkWidget *lblIndexScan;
-	GtkWidget *lblInfo;
-	GtkWidget *btnIndexScan;
-	RelOptInfo *rel;
-	char * buff;
-
-	buff = malloc(sizeof(char)*CSTR_BUFF);
-	rel = idxscan->parent;
-	cstr_rel(buff, 0, CSTR_BUFF, state->root, rel);
-	printf("Index Scan\n");
-	printf("%s\n", buff);
-
-	grid = (GtkGrid*)gtk_grid_new();
-	lblIndexScan = gtk_label_new( "Index Scan" );
-	lblInfo = gtk_label_new( buff );
-	btnIndexScan = gtk_button_new_with_label("update");
-	g_signal_connect(G_OBJECT(btnIndexScan), "clicked", G_CALLBACK(btn_clicked), NULL);
-	gtk_grid_attach(grid, lblIndexScan, 0,0,1,1);
-	gtk_grid_attach(grid, lblInfo, 0,1,1,1);
-	gtk_grid_attach(grid, btnIndexScan, 0,2,1,1);
-
-	gtk_widget_show(GTK_WIDGET(lblIndexScan));
-	gtk_widget_show(GTK_WIDGET(lblInfo));
-	gtk_widget_show(GTK_WIDGET(btnIndexScan));
-	gtk_widget_show(GTK_WIDGET(grid));
-	return GTK_WIDGET(grid);
+	return GTK_WIDGET(
+			create_widget_from_path(state, idxscan, "Index Scan", &btn_clicked)
+		);
 }
 
+static GtkWidget * make_join_node_widget(UIState* state, JoinPath* joinpath) {
+	switch(joinpath->path.pathtype){
+		case T_HashJoin:
+			return create_hash_join_widget(state, joinpath);
+		case T_MergeJoin:
+			return create_merge_join_widget(state, joinpath);
+		case T_NestLoop:
+			return create_nested_loop_widget(state, joinpath);
+		default:
+			Assert(false);
+			return NULL;
+	}
+}
+
+static GtkWidget * make_leaf_node_widget(UIState* state, Path* path) {
+	switch(path->pathtype){
+			case T_SeqScan:
+				return create_seq_scan_widget(state, path);
+			case T_IndexOnlyScan :
+			case T_IndexScan :
+				return create_idx_scan_widget(state,path);
+			default:
+				Assert(false);
+				return NULL;
+	}
+}
+
+static void setup_grid_recurse(UIState * state, Path* path, GtkGrid *grid, int r, int c, int height) {
+	if(isJoinPath(path)) {
+		JoinPath* joinpath;
+		joinpath = (JoinPath*)path;
+		gtk_grid_attach(grid, make_join_node_widget(state,joinpath), c,r,1,1);
+		setup_grid_recurse(state, joinpath->outerjoinpath, grid, r+1, c-1, height-1);
+		setup_grid_recurse(state, joinpath->innerjoinpath, grid, r+1, c+1, height-1);
+	} else if(isLeafPath(path)) {
+		gtk_grid_attach(grid, make_leaf_node_widget(state,path), c,r,1,1);
+	}
+}
 
 static void setup_grid(GtkWidget *window, UIState * state) {
 	GtkGrid *grid;
-
-	state->rows = 2;
-	state->cols = 3;
+	int col;
 	grid = (GtkGrid*)gtk_grid_new();
 
-	if(state->height != 1) {
-		//TODO: support 1 join
-		//TODO: support 2 joins
-		return;
+	if(state->height == 1) {
+		col = 0;
+	} else if(state->height == 2) {
+		col = 1;
+	} else {
+		col = 3;
 	}
 
-	switch((*state->cheapest_path)->pathtype){
-			case T_SeqScan:
-				gtk_grid_attach(grid, create_seq_scan_widget(state,*state->cheapest_path), 0,0,1,1);
-				break;
-			case T_IndexOnlyScan :
-			case T_IndexScan :
-				gtk_grid_attach(grid, create_idx_scan_widget(state,*state->cheapest_path), 0,0,1,1);
-				break;
-			default:
-				return;
-	}
+	setup_grid_recurse(state, *state->cheapest_path, grid, 0, col, state->height);
 
 	gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(grid));
 	gtk_widget_show(GTK_WIDGET(grid));
